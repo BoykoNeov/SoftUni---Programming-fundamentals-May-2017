8c21d4804dffe6b98d2ec74afc5513b736ea562b
BoykoNeov <boikoneov@gmail.com>
2017-04-03 10:45:52 +0300

    Adding initial work on "Arrays and Lists"

---
+++ ArraysAndlists/RotateArray/RotateArray.cs
@@ -0,0 +1,23 @@
+﻿// Program for rotating arrays using different Algorithms
+
+using System;
+using System.Linq;
+
+public class RotateArray
+{
+    public static void Main()
+    {
+ // Reads ints from the Console and converts them to an array of ints
+
+        Console.WriteLine("Please enter array of integers (integers separated by spaces)");
+        var intArray = Console.ReadLine()
+            .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+        // Alternative syntaxis without Linq
+        //var intArray = Array.ConvertAll(Console.ReadLine()
+        //    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries), int.Parse);
+
+       
+
+    }
+}
