af68fe3fe5a082fa904e1f45006720349a082237
BoykoNeov <boikoneov@gmail.com>
2017-04-05 00:25:26 +0300

    Making alternative program for rotating string arrays with more separating characters in the input and without console instructions to the user

--- ArraysAndlists/RotateArray/RotateIntArray.cs
+++ ArraysAndlists/RotateArray/RotateIntArray.cs
@@ -3,7 +3,7 @@
 using System;
 using System.Linq;
 
-public class RotateArray
+public class RotateIntArray
 {
     public static void Main()
     {
---
+++ ArraysAndlists/RotateStringArrayJuggling/RotateStringArrayJuggling.cs
@@ -0,0 +1,69 @@
+﻿// Program for rotating arrays using different Algorithms
+
+using System;
+using System.Linq;
+
+public class RotateStringArrayJuggling
+{
+    public static void Main()
+    {
+        // Reads strings from the Console and converts them to an array
+        // Console.WriteLine("Please enter array of strings (integers separated by spaces)");
+        string[] intArray = Console.ReadLine()
+            .Split(new char[] { ' ', ';' , '!', ',','?','.','/', '\\' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+        // Alternative syntaxis without Linq
+        // var intArray = Array.ConvertAll(Console.ReadLine()
+        //    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries), int.Parse);
+
+        //Console.WriteLine("Enter the number of positions to be shifted");
+        //Console.WriteLine("Enter positive number for right shift, negative for left");
+        int d = int.Parse(Console.ReadLine()) % intArray.Length;
+        if (d < 0)
+        {
+            d = (d + intArray.Length) % intArray.Length;
+        }
+
+        if (d != 0)
+        {
+            SubsetRotate(intArray, d);
+        }
+
+        Console.WriteLine(string.Join(" ", intArray));
+    }
+
+    public static void SubsetRotate(string[] array, int d)
+    {
+        int arraySubsetsNumber = EuclidsAlgorithm(array.Length, d);
+        for (int i = 0; i < arraySubsetsNumber; i++)
+        {
+            for (int k = 0; k < array.Length / arraySubsetsNumber; k++)
+            {
+                int positionBase = i;
+                int positionSwitch = (i + (k * d) + d) % array.Length;
+                if (positionSwitch == positionBase)
+                {
+                    break;
+                }
+
+                string temp = array[positionBase];
+                array[positionBase] = array[positionSwitch];
+                array[positionSwitch] = temp;
+            }
+        }
+    }
+
+    // Euclidian algorithm to determine the greatest common divisor
+    public static int EuclidsAlgorithm(int m, int n)
+    {
+        m = m % n;
+        if (m == 0)
+        {
+            return n;
+        }
+        else
+        {
+            return EuclidsAlgorithm(n, m);
+        }
+    }
+}
