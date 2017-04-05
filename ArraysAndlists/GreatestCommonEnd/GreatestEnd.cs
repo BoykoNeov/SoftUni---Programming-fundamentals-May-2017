ec4bdef706353d669ac20097165b583af13e5f8e
BoykoNeov <boikoneov@gmail.com>
2017-04-05 12:46:21 +0300

    Work in progress on Arrays - Excercises

---
+++ ArraysAndlists/GreatestCommonEnd/GreatestEnd.cs
@@ -0,0 +1,51 @@
+﻿// Read two arrays of words and find the length of the largest common end (left or right).
+using System;
+using System.Linq;
+
+public class GreatestEnd
+{
+    public static void Main()
+    {
+        var firstString = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+        var secondString = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+        // the common elements can't be bigger than any of the arrays , so this array can be as big as any of the two arrays
+        string[] beginningCommonEnd = new string[firstString.Length];
+
+        // search the strings from beginning to end for a common end and the beginning
+        for (int i = 0; i < firstString.Length && i < secondString.Length; i++)
+        {
+            if (firstString[i] == secondString[i])
+            {
+                beginningCommonEnd[i] = firstString[i];
+            }
+            else
+            {
+                break;
+            }
+        }
+        // since the string[] was defined as probably bigger than the answer will be, remove empty entries in order to properly determine its length
+        beginningCommonEnd = beginningCommonEnd.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+        // define the back end and search the strings[] from end to beginning
+        string[] endingCommonEnd = new string[firstString.Length];
+        for (int j = 0; j < firstString.Length && j < secondString.Length; j++)
+        {
+            if (firstString[firstString.Length - 1 - j] == secondString[secondString.Length - 1 - j])
+            {
+                endingCommonEnd[firstString.Length - 1 - j] = secondString[secondString.Length - 1 - j];
+            }
+            else
+            {
+                break;
+            }
+        }
+        // as with the other end, remove the empty entries in order to properly determine the length
+        endingCommonEnd = endingCommonEnd.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+        // compare the two and display the length of the bigger
+        if (endingCommonEnd.Length > beginningCommonEnd.Length)
+        {
+            beginningCommonEnd = endingCommonEnd;
+        }
+
+        Console.WriteLine(beginningCommonEnd.Length);
+    }
+}
---
+++ ArraysAndlists/RotateAndSum/RotateAndSum.cs
@@ -0,0 +1,35 @@
+﻿// Write a program to read an array of n integers (space separated on a single line) and an integer k,
+// rotate the array right k times and sum the obtained arrays after each rotation
+
+using System;
+using System.Linq;
+
+public class RotateAndSum
+{
+    public static void Main()
+    {
+        int[] array = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        int rotationCount = int.Parse(Console.ReadLine());
+        int[] rotatedArray = new int[array.Length];
+        int[] sumArray = new int[array.Length];
+
+        for (int j = 0; j < rotationCount; j++)
+        {
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                rotatedArray[(i + 1) % array.Length] = array[i];
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rotatedArray[i];
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                sumArray[i] += rotatedArray[i];
+            }
+        }
+
+        Console.WriteLine(String.Join(" ", sumArray));
+    }
+}
