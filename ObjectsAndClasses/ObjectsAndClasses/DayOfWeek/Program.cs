543df9c056e813adb2636ab226b33ec80414c36e
BoykoNeov <boikoneov@gmail.com>
2017-04-17 13:41:54 +0300

    Started working on excercises from "Objects and classes"

---
+++ ObjectsAndClasses/ObjectsAndClasses/DayOfWeek/Program.cs
@@ -0,0 +1,13 @@
+﻿// You are given a date in format day-month-year. Calculate and print the day of week in English.
+
+using System;
+using System.Globalization;
+
+public class DayOfWeek
+{
+    public static void Main()
+    {
+        DateTime date = DateTime.ParseExact(Console.ReadLine(), "d-M-yyyy", CultureInfo.InvariantCulture);
+        Console.WriteLine(date.DayOfWeek);
+    }
+}
