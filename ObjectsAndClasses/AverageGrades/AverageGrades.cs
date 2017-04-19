30f174e8e1cc10f5de2bb242d098f77b3d647c4e
BoykoNeov <boikoneov@gmail.com>
2017-04-19 07:29:42 +0300

    Finished tasks "Average Grades" and "Advertisement Message"

---
+++ ObjectsAndClasses/AverageGrades/AverageGrades.cs
@@ -0,0 +1,30 @@
+﻿// Task "Average Grades", for details check ../07. Programming-Fundamentals-Objects-and-Classes-Exercises.docx
+
+using System;
+using System.Linq;
+
+namespace AverageGrades
+{
+    public class AverageGrades
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            Students[] studentsList = new Students[n];
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+                studentsList[i] = new Students()
+                {
+                    Name = input[0],
+                    Grades = input.Skip(1).Select(double.Parse).ToArray()
+                };
+            }
+
+            foreach (Students student in studentsList.Where(x => x.Average >= 5).OrderBy(x => x.Name).ThenByDescending(x => x.Average))
+            {
+                Console.WriteLine($"{student.Name} -> {student.Average:f2}");
+            }
+        }
+    }
+}
---
+++ ObjectsAndClasses/AverageGrades/Students.cs
@@ -0,0 +1,19 @@
+﻿// Class for holding student information
+
+using System.Linq;
+
+namespace AverageGrades
+{
+    public class Students
+    {
+        public string Name { get; set; }
+        public double[] Grades { get; set; }
+        public double Average
+        {
+            get
+            {
+                return Grades.Average();
+            }
+        }
+    }
+}
