76c30080a3231e2253dae5d571d85fcf6adef418
BoykoNeov <boikoneov@gmail.com>
2017-04-20 16:30:12 +0300

    Finished task "Andrey and Billiard". Visual studio insisted of normalizing line endings of some files, reason for this occuring unknown - maybe some difference with visual studio 2017, which I use on my other comp

---
+++ ObjectsAndClasses/AndreyAndBilliard/AndreyAndBilliard.cs
@@ -0,0 +1,90 @@
+﻿// Task "Andrey And Billiard", for details check ../07. Programming-Fundamentals-Objects-and-Classes-Exercises.docx
+
+namespace AndreyAndBilliard
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class AndreyAndBilliard
+    {
+        public static void Main()
+        {
+            Dictionary<string, decimal> goods = new Dictionary<string, decimal>();
+            int goodsCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < goodsCount; i++)
+            {
+                string[] priceInputs = Console.ReadLine().Split(new char[] { '-' }, StringSplitOptions.RemoveEmptyEntries);
+                goods[priceInputs[0]] = decimal.Parse(priceInputs[1]);
+            }
+
+            var customerList = new List<Customer>();
+
+            while (true)
+            {
+                string primaryCustomersInput = Console.ReadLine();
+                if (primaryCustomersInput == "end of clients")
+                {
+                    break;
+                }
+
+                string[] customersInput = primaryCustomersInput
+                  .Split(new char[] { ',', '-' }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (!goods.ContainsKey(customersInput[1]))
+                {
+                    continue;
+                }
+
+                bool customerExists = false;
+                int customerNumber = 0;
+                for (int i = 0; i < customerList.Count; i++)
+                {
+                    if (customersInput[0] == customerList[i].Name)
+                    {
+                        customerExists = true;
+                        customerNumber = i;
+                    }
+                }
+
+                if (!customerExists)
+                {
+                    customerList.Add(new Customer());
+                    customerList.Last().Name = customersInput[0];
+                    customerList.Last().Buys = new Dictionary<string, int>();
+                    customerList.Last().Buys.Add(customersInput[1], int.Parse(customersInput[2]));
+                }
+                else
+                {
+                    if (customerList[customerNumber].Buys.ContainsKey(customersInput[1]))
+                    {
+                        customerList[customerNumber].Buys[customersInput[1]] += int.Parse(customersInput[2]);
+                    }
+                    else
+                    {
+                        customerList[customerNumber].Buys.Add(customersInput[1], int.Parse(customersInput[2]));
+                    }
+                }
+            }
+
+            decimal totalBill = 0;
+            foreach (var client in customerList.OrderBy(x => x.Name))
+            {
+                decimal totalCustomerBill = 0;
+                Console.WriteLine(client.Name);
+
+                foreach (var purchases in client.Buys)
+                {
+                    totalCustomerBill += purchases.Value * goods[purchases.Key];
+                    Console.WriteLine($"-- {purchases.Key} - {purchases.Value}");
+                }
+
+                totalBill += totalCustomerBill;
+                Console.WriteLine($"Bill: {totalCustomerBill:f2}");
+            }
+
+            Console.WriteLine($"Total bill: {totalBill:f2}");
+        }
+    }
+}
---
+++ ObjectsAndClasses/AndreyAndBilliard/Customer.cs
@@ -0,0 +1,13 @@
+﻿// Class for holding customer Name and Buys
+
+namespace AndreyAndBilliard
+{
+    using System.Collections.Generic;
+
+   public class Customer
+    {
+       public string Name { get; set; }
+
+       public Dictionary<string, int> Buys { get; set; }
+    }
+}
