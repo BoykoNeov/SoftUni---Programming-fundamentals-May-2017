f4d1df676ab6635411b1aaa492339941f8a2ac72
BoykoNeov <boikoneov@gmail.com>
2017-04-25 12:04:26 +0300

    Some playing around with "Files and Exceptions"

---
+++ FilesAndExceptions/FactorialToFile/FactorialToFile.cs
@@ -0,0 +1,28 @@
+﻿// Calculate factorial from a number and write the result to a file
+
+using System;
+using System.IO;
+using System.Numerics;
+
+public class FactorialToFile
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter output file name");
+        string fileName = Console.ReadLine();
+
+        Console.WriteLine("enter n!");
+        BigInteger number = BigInteger.Parse(Console.ReadLine());
+        File.AppendAllText (fileName, Convert.ToString(CalculateFactorial(number)));
+    }
+
+    public static BigInteger CalculateFactorial(BigInteger n)
+    {
+        BigInteger result = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            result *= i;
+        }
+        return result;
+    }
+}
---
+++ FilesAndExceptions/PrimesInRangeToFile/PrimesInRangeToFile.cs
@@ -0,0 +1,68 @@
+﻿// Write all primes in a range to file
+
+using System;
+using System.Text;
+using System.IO;
+
+public class PrimesInRangeToFile
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter the two numbers between which to find all prime numbers on separate lines");
+        Console.WriteLine("(Second number must be bigger than the first, minimum valie of both number - \"2\"!)");
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the name of the output file:");
+        string outputFile = Console.ReadLine();
+
+        if (a == 1) { }
+        string output = Primes(a, b);
+        File.AppendAllText(outputFile, output);
+    }
+
+    public static string Primes(int a, int b)
+    {
+        if (a < 2)
+        {
+            a = 2;
+        }
+
+        if (b < 2)
+        {
+            b = 2;
+        }
+
+        StringBuilder output = new StringBuilder();
+        for (int i = a; i <= b; i++)
+        {
+            bool isPrime = true;
+            for (int j = 2; j <= Math.Sqrt(i); j++)
+            {
+                if (i % j == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime)
+            {
+                output.Append(i).Append(", ");
+            }
+        }
+
+        output.Remove(output.Length - 2, 2);
+        return (output.ToString());
+    }
+}
+
+
+
+
+
+
+
+
+
+
