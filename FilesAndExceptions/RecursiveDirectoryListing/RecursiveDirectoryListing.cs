435bcb2db38eaec3803c6f8c112c6fdf58c2cacd
BoykoNeov <boikoneov@gmail.com>
2017-04-26 22:08:22 +0300

    Started work on recursive direcory lister

---
+++ FilesAndExceptions/RecursiveDirectoryListing/RecursiveDirectoryListing.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace RecursiveDirectoryListing
+{
+    public class RecursiveDirectoryListing
+    {
+        public static void Main(string args)
+        {
+            DirectoryInfo currentDirectory = new DirectoryInfo(Directory.GetCurrentDirectory());
+            
+            if (args != null)
+            {
+                currentDirectory = new  DirectoryInfo(args);
+            }
+
+
+
+            var subdirecories = Directory.GetDirectories(currentDirectory.FullName).ToList();
+
+
+        }
+
+        public static List<string> RecurseList(List<string> inputDir)
+        {
+            return inputDir;
+        }
+    }
+}
