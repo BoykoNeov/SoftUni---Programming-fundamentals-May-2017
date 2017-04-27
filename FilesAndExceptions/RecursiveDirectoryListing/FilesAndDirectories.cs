84f31731160db6182b574bfe3c604a72f5f9df83
BoykoNeov <boikoneov@gmail.com>
2017-04-27 06:10:27 +0300

    Continuing work on Recursive Directory and Files Lister

---
+++ FilesAndExceptions/RecursiveDirectoryListing/FilesAndDirectories.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace RecursiveDirectoryListing
+{
+    using System.Collections.Generic;
+
+    public class FilesAndDirectories
+    {
+        public List<string> DirectoriesList { get; set; }
+        public List<string> FilesAndDirectoriesList { get; set; }
+        string prefix { get; set; }
+    }
+}
--- FilesAndExceptions/RecursiveDirectoryListing/RecursiveDirectoryListing.cs
+++ FilesAndExceptions/RecursiveDirectoryListing/RecursiveDirectoryListing.cs
@@ -1,31 +1,97 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿// Program to recursively list all directories and files in a given path
 
 namespace RecursiveDirectoryListing
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
     public class RecursiveDirectoryListing
     {
-        public static void Main(string args)
+        public static void Main(string[] args)
         {
             DirectoryInfo currentDirectory = new DirectoryInfo(Directory.GetCurrentDirectory());
-            
+
             if (args != null)
             {
-                currentDirectory = new  DirectoryInfo(args);
+                //   currentDirectory = new  DirectoryInfo(args[0]);
             }
 
 
 
-            var subdirecories = Directory.GetDirectories(currentDirectory.FullName).ToList();
+            //var subdirecories = Directory.GetDirectories(@"c:\", "*", SearchOption.TopDirectoryOnly).ToList();
+            //Console.WriteLine(string.Join(Environment.NewLine, subdirecories));
+            //subdirecories = Directory.GetFiles(@"c:\").ToList();
+            //for (int i = 0; i < subdirecories.Count; i++)
+            //{
+            //    subdirecories[i] = Path.GetFileName(subdirecories[i]);
+            //}
 
+            //Console.WriteLine(string.Join(Environment.NewLine, subdirecories));
+
+            var list2 = RecurseList(currentDirectory.Parent.Parent.FullName);
+            Console.WriteLine(string.Join(Environment.NewLine, list2.FilesAndDirectoriesList));
+            Console.WriteLine("--------------------------------------------------------------");
+            Console.WriteLine("--------------------------------------------------------------");
+            Console.WriteLine(string.Join(Environment.NewLine, list2.DirectoriesList));
 
         }
 
-        public static List<string> RecurseList(List<string> inputDir)
+        public static FilesAndDirectories RecurseList(string inputDir)
         {
-            return inputDir;
+            FilesAndDirectories currentObject = new FilesAndDirectories()
+            {
+                DirectoriesList = new List<string>(),
+                FilesAndDirectoriesList = new List<string>()
+            };
+
+            var subDirectories = Directory.GetDirectories(inputDir).ToList();
+
+            foreach (string folder in subDirectories)
+            {
+                currentObject.DirectoriesList.Add(folder);
+                currentObject.FilesAndDirectoriesList.Add(folder);
+                currentObject.FilesAndDirectoriesList.AddRange(Directory.GetFiles(folder)
+                    .Select(Path.GetFileName)
+                    .Select(x => "  **  " + x)
+                    .ToList());
+
+                FilesAndDirectories tempObject = new FilesAndDirectories();
+                tempObject = RecurseList(folder);
+                currentObject.DirectoriesList.AddRange(tempObject.DirectoriesList);
+                currentObject.FilesAndDirectoriesList.AddRange(tempObject.FilesAndDirectoriesList);
+            }
+
+
+            return currentObject;
+
+            //try
+            //{
+            //    currentObject.DirectoriesList = Directory.GetDirectories(inputDir).ToList();
+
+            //    for (int i = 0; i < currentObject.DirectoriesList.Count; i++)
+            //    {
+            //        var directory = currentObject.DirectoriesList[i];
+            //        currentObject.DirectoriesList.AddRange(RecurseList(directory).DirectoriesList);
+            //        currentObject.FilesAndDirectoriesList = new List<string>(currentObject.DirectoriesList);
+            //        List<string> a = Directory.GetFiles(inputDir).Select(Path.GetFileName).ToList();
+            //        currentObject.FilesAndDirectoriesList.AddRange(a);
+
+            //    }
+            //}
+            //catch
+            //{
+            //    return currentObject;
+            //}
+
+//            currentObject.FilesAndDirectoriesList = new List<string>(currentObject.DirectoriesList);
+
+            //List<string> a = Directory.GetFiles(inputDir).Select(Path.GetFileName).ToList();
+
+            //currentObject.FilesAndDirectoriesList.AddRange(a);
+
+            return currentObject;
         }
     }
 }
