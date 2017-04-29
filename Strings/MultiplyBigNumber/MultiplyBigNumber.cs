b6e042665ad6bf06f059f53819d9a9697555dfc6
BoykoNeov <boikoneov@gmail.com>
2017-04-29 22:08:16 +0300

    Minor changes and comments to "Multiply big numbers". Finished task "Sum Big Numbers"

---
+++ Strings/MultiplyBigNumber/MultiplyBigNumber.cs
@@ -0,0 +1,52 @@
+﻿namespace MultiplyBigNumber
+{
+    using System;
+
+    /// <summary>
+    ///  You are given two lines - the first one can be a really big number (0 to 10^50). 
+    /// The second one will be a single digit number (0 to 9). You must display the product of these numbers.
+    /// Note: do not use the BigInteger or BigDecimal classes for solving this problem.
+    /// </summary>
+    public class MultiplyBigNumber
+    {
+        public static void Main()
+        {
+            string inputNumber = Console.ReadLine();
+            int multiplier = int.Parse(Console.ReadLine());
+            const int inputNumberMaxDigits = 52;
+            int[] resultOutput = new int[inputNumberMaxDigits];
+
+            for (int i = 0; i < inputNumber.Length; i++)
+            {
+                // 48 is the ASCII value of zero
+                int multiplicator = (inputNumber[inputNumber.Length - 1 - i] - 48);
+                int multiplicationResult = multiplicator * multiplier;
+
+                int positionResult = multiplicationResult % 10;
+                int carry = multiplicationResult / 10;
+
+                resultOutput[inputNumberMaxDigits - 1 - i] += positionResult;
+                resultOutput[inputNumberMaxDigits - 2 - i] += carry;
+            }
+
+            for (int i = resultOutput.Length - 1; i > 0; i--)
+            {
+                if (resultOutput[i] > 9)
+                {
+                    resultOutput[i - 1] += resultOutput[i] / 10;
+                    resultOutput[i] = resultOutput[i] % 10;
+                }
+            }
+
+            string outputString = String.Join("", resultOutput).TrimStart('0');
+            if (outputString.Length > 0)
+            {
+                Console.WriteLine(outputString);
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
---
+++ Strings/SumBigNumbers/SumBigNumbers.cs
@@ -0,0 +1,72 @@
+﻿namespace SumBigNumbers
+{
+    using System;
+
+    /// <summary>
+    /// You are given two lines - each can be a really big number (0 to 1050). You must display the sum of these numbers.
+    ///Note: do not use the BigInteger or BigDecimal classes for solving this problem.
+    /// </summary>
+    public class SumBigNumbers
+    {
+        public static void Main()
+        {
+            string firstNumber = Console.ReadLine();
+            string secondNumber = Console.ReadLine();
+            const int maxResultSize = 51;
+            int[] result = new int[maxResultSize];
+
+            // Ensures that the first number is bigger ot equal in length to the second
+            if (secondNumber.Length > firstNumber.Length)
+            {
+                string temp = new string(secondNumber.ToCharArray());
+                secondNumber = new string(firstNumber.ToCharArray());
+                firstNumber = new string(temp.ToCharArray());
+            }
+
+            for (int i = 0; i < firstNumber.Length || i < secondNumber.Length; i++)
+            {
+                int currentDigit = 0;
+                int carryDigit = 0;
+
+                // 48 is the ascii table value of 0
+                int firstNumberDigit = firstNumber[firstNumber.Length - 1 - i] - 48;
+
+                // checks if the second number has "run" out of digits
+                if (secondNumber.Length - 1 - i >= 0)
+                {
+                    int secondNumberDigit = secondNumber[secondNumber.Length - 1 - i] - 48;
+                    currentDigit = (firstNumberDigit + secondNumberDigit) % 10;
+                    carryDigit = (firstNumberDigit + secondNumberDigit) / 10;
+                }
+                else
+                {
+                    currentDigit = firstNumberDigit;
+                    carryDigit = 0;
+                }
+
+                result[result.Length - 1 - i] += currentDigit;
+                result[result.Length - 2 - i] += carryDigit;
+            }
+
+            // runs through the sequence and "fixes" the places where a carry is occuring
+            for (int i = result.Length - 1; i > 0; i--)
+            {
+                if (result[i] > 9)
+                {
+                    result[i - 1] += result[i] / 10;
+                    result[i] = result[i] % 10;
+                }
+            }
+
+            string outputString = String.Join("", result).TrimStart('0');
+            if (outputString.Length > 0)
+            {
+                Console.WriteLine(outputString);
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
