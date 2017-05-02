a56f6a533daffb711ef7bed2772918b785713f6d
BoykoNeov <boikoneov@gmail.com>
2017-05-02 03:30:47 +0300

    Started work on "Regex" lab and excercises. Finished task "Replace <a> Tag"

---
+++ Regex/ReplaceATag/ReplaceATag.cs
@@ -0,0 +1,30 @@
+﻿/// <summary>
+/// Write a program that replaces in a HTML document given as string all the tags <a href=…>…</a> 
+/// with corresponding tags [URL href=…>…[/URL]. Read an input, until you receive “end” command. Print the result on the console. 
+/// </summary>
+namespace ReplaceATag
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    class ReplaceATag
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            string HTMLdocument = string.Empty;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                HTMLdocument = input;
+
+                Regex HTMLtags = new Regex(@"(<a ?)(.+)(>)(.+)(<\/a>)");
+                Match match = HTMLtags.Match(HTMLdocument);
+
+                //   Console.WriteLine(match.Groups[2].Value);
+                string output = HTMLtags.Replace(HTMLdocument, @"[URL $2]$4[/URL]");
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
