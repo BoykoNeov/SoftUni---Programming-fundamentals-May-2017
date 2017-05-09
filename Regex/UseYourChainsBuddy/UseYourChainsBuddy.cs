f105ca37b45ca344f86da7eafca4b8b27faaddd7
BoykoNeov <boikoneov@gmail.com>
2017-05-09 13:36:43 +0300

    Started work on task "Use Your Chains Buddy"

---
+++ Regex/UseYourChainsBuddy/UseYourChainsBuddy.cs
@@ -0,0 +1,40 @@
+﻿/// <summary>
+///  *Use Your Chains, Buddy
+/// This problem is from the JavaScript Basics Exam(9 January 2015). You may check your solution here.
+/// You are in Cherny Vit now and there are 12km to Anchova Bichkiya Hut.You need to get there by car.But there is so much snow that you need to use car chains. In order to put them on the wheels correctly, you need to read the manual. But it is encrypted…
+/// As input you will receive an HTML document as a single string. You need to get the text from all the<p> tags and replace all characters which are not small letters and numbers with a space " ". After that you must decrypt the text – all letters from a to m are converted to letters from n to z (a  n; b  o; … m  z). The letters from n to z are converted to letters from a to m(n  a; o  b; … z  m). All multiple spaces should then be replaced by only one space.
+/// For example, if we have "<div>Santa</div><p>znahny # grkg ()&^^^&12</p>" we extract "znahny # grkg ()&^^^&12". Every character that is not a small letter or a number is replaced with a space("znahny grkg       12"). We convert each small letter as described above("znahny grkg       12"  "manual text       12") and replace all multiple spaces with only one space("manual text 12"). Finally, we concatenate the decrypted text from all<p></p> tags(in this case, it's only one). And there you go – you have the manual ready to read!
+/// Input:
+/// The input is read from the console and consists of just one line – the string with the HTML document.
+/// The input data will always be valid and in the format described.There is no need to check it explicitly.
+/// Output:
+/// Print on a single line the decrypted text of the manual. See the given examples below.
+/// Constraints:
+///	Allowed working time: 0.2 seconds.Allowed memory: 16 MB.
+/// </summary>
+namespace UseYourChainsBuddy
+{
+    using System;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    public class UseYourChainsBuddy
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            Regex pTags = new Regex(@"(?<=<p>).*?(?=<\/p>)");
+            MatchCollection matches = pTags.Matches(input);
+            StringBuilder currentState = new StringBuilder();
+            foreach (Match match in matches)
+            {
+                currentState.Append(match);
+                currentState.Append(" ");
+            }
+            Console.WriteLine(currentState.ToString());
+            Regex nonSmallLettersOrNumbers = new Regex (@"[^a-z0-9]+");
+
+
+        }
+    }
+}
