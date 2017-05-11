57e7243a0a5ee2010b796a6af5e424789fba75e9
BoykoNeov <boikoneov@gmail.com>
2017-05-11 19:10:44 +0300

    :mushroom: Finished tasks "Endurance Rally" and "Winning ticket"

---
+++ ExamPreparation/Exam Preparation I - Exam 7 Jan 2017/Exam-7Jan2017/EnduranceRally/EnduranceRally.cs
@@ -0,0 +1,85 @@
+﻿/// <summary>
+/// The goal of the Endurance Rally is, simply, to finish the race.
+/// You are given the names of the participants, the track layout and the** checkpoint** indexes.
+/// The starting fuel of each participant is equal to the ASCII code of the first character of his name.
+/// Track layout consists of zones represented by numbers, given on a single line separated by a single space. Every number represents the fuel given or the fuel taken by the current zone of the track:
+/// If the current zone is a checkpoint, the value of the zone is added to the driver's fuel.
+/// If the current zone is not a checkpoint , the value of the zone is subtracted from the driver's fuel.
+/// Zones are indexed.Indexes are sequential and always start from zero (like an array).
+/// The checkpoints are numbers, representing indexes that show if a zone of the track** is a checkpoint**. For example, you are given checkpoints 0, 3 and 5, that means that zones at index 0, 3 and 5 of the track are checkpoints and therefore provide fuel for the driver.
+/// Given this information, you need to check if a driver can finish and print the fuel that he is left with. If a driver can't finish you need to print the zone he managed to reach.
+/// Input
+/// The input will be read from the console.The input consists of exactly three lines :
+/// The first line holds the drivers' names separated by a space: "{driver 1} {driver 2} … {driver N}"
+/// On the second line is the track layout (zones) in the form of numbers separated by a space: "{zone 0} {zone 1} … {zone N}"
+/// On the third line are the checkpoint indexes also separated by a space: "{index 0} {index 1} … {index N}"
+/// Output
+/// Print all drivers in the order of their appearance , each on a separate line:
+/// If the driver finished, print his name and fuel left to the second digit after the decimal point in the format: "{driver name} - fuel left {fuel points}"
+/// If the driver could not finish, print: "{driver name} - reached {zone index}"
+/// Constrains
+/// Drivers count will be in the range [0 … 200]
+/// Zone fuel will be a floating-point number
+/// </summary>
+namespace EnduranceRally
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class EnduranceRally
+    {
+        public static void Main()
+        {
+            string[] drivers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            double[] zones = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse).ToArray();
+
+            int[] checkpointsInput = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            var checkpoints = new HashSet<int>();
+            foreach (int checkpont in checkpointsInput)
+            {
+                checkpoints.Add(checkpont);
+            }
+
+            foreach (string driver in drivers)
+            {
+                double driverFuel = driver[0];
+                int currentZone = 0;
+
+                for (int i = 0; i < zones.Length; i++)
+                {
+                    currentZone = i;
+                    if (checkpoints.Contains(i))
+                    {
+                        driverFuel += zones[i];
+                    }
+                    else
+                    {
+                        driverFuel -= zones[i];
+                    }
+
+                    if (driverFuel <= 0)
+                    {
+                       break;
+                    }
+                }
+
+                if (driverFuel > 0)
+                {
+                    Console.WriteLine($"{driver} - fuel left {driverFuel:f2}");
+                }
+                else
+                {
+                    Console.WriteLine($"{driver} - reached {currentZone}");
+                }
+            }
+        }
+    }
+}
---
+++ ExamPreparation/Exam Preparation I - Exam 7 Jan 2017/Exam-7Jan2017/WinningTicket/WinningTicket.cs
@@ -0,0 +1,70 @@
+﻿/// <summary>
+/// You are given a collection of tickets separated by commas and spaces. You need to check every one of them if it has a winning combination of symbols.
+/// A valid ticket should have exactly 20 characters.The winning symbols are ' @', ' #', ' $' and ' ^'. But in order for a ticket to be a winner the symbol should uninterruptedly repeat for at least 6 times in both the tickets left half and the tickets right half.
+/// For example, a valid winning ticket should be something like this:
+/// " Cash$$$$$$Ca$$$$$$sh"
+/// The left half " Cash$$$$$$" contains " $$$$$$", which is also contained in the tickets right half " Ca$$$$$$sh". A winning ticket should contain symbols repeating up to 10 times in both halves, which is considered a Jackpot (for example: " $$$$$$$$$$$$$$$$$$$$").
+/// Input
+/// The input will be read from the console.The input consists of a single line containing all tickets separated by commas and one or more white spaces in the format:
+/// "{ticket}, {ticket}, … {ticket}"
+/// Output
+/// Print the result for every ticket in the order of their appearance, each on a separate line in the format:
+/// Invalid ticket - "invalid ticket"
+/// No match - "ticket "{ticket}" - no match"
+/// Match with length 6 to 9 - "ticket "{ticket}" - {match length}{match symbol}"
+/// Match with length 10 - "ticket "{ticket}" - {match length}{match symbol} Jackpot!"
+/// Constrains
+/// Number of tickets will be in range[0 … 100]
+/// </summary>
+namespace WinningTicket
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class WinningTicket
+    {
+        public static void Main()
+        {
+            string[] tickets = Console.ReadLine().Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+            Regex ticketValidator = new Regex(@"([@#\$\^]){1}\1{5,9}");
+            foreach (string ticket in tickets)
+            {
+                if (ticket.Length != 20)
+                {
+                    Console.WriteLine("invalid ticket");
+                    continue;
+                }
+
+                string leftPart = ticket.Substring(0,10);
+                string rightPart = ticket.Substring(10, 10);
+
+                var leftWinningCombination = ticketValidator.Match(leftPart).ToString();
+                var rightWinningCombination = ticketValidator.Match(rightPart).ToString();
+
+                int winningLengthInBoth = 0;
+                winningLengthInBoth = leftWinningCombination.Length;
+                if (rightWinningCombination.Length < leftWinningCombination.Length)
+                {
+                    winningLengthInBoth = rightWinningCombination.Length;
+                }
+
+                // The Judge gives 100 out of 100 with both the following condition commented out and not, but if I interpret the task
+                // definiiton correctly this checked should be applied, still leaving it commented out for now
+                if (winningLengthInBoth >= 6) // && (leftWinningCombination[0] == rightWinningCombination[0]))
+                {
+                    string jackpotOutput = string.Empty;
+                    if (winningLengthInBoth == 10)
+                    {
+                        jackpotOutput = " Jackpot!";
+                    }
+
+                    Console.WriteLine($"ticket \"{ticket}\" - {winningLengthInBoth}{leftWinningCombination[0]}" + jackpotOutput);
+                }
+                else
+                {
+                    Console.WriteLine($"ticket \"{ticket}\" - no match");
+                }
+            }
+        }
+    }
+}
