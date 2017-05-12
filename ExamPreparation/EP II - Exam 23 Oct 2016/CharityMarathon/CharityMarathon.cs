77fe5260dd0e447fc237eab23c76a350a227b375
BoykoNeov <boikoneov@gmail.com>
2017-05-12 17:49:05 +0300

    :snowflake: Started work on task "Charity Marathon"

---
+++ ExamPreparation/EP II - Exam 23 Oct 2016/CharityMarathon/CharityMarathon.cs
@@ -0,0 +1,21 @@
+﻿namespace CharityMarathon
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class CharityMarathon
+    {
+        public static void Main()
+        {
+            int days = int.Parse(Console.ReadLine());
+            int runners = int.Parse(Console.ReadLine());
+            int laps = int.Parse(Console.ReadLine());
+            int trackLength = int.Parse(Console.ReadLine());
+            int trackCapacity = int.Parse(Console.ReadLine());
+            decimal moneyPerKilometer = decimal.Parse(Console.ReadLine());
+        }
+    }
+}
