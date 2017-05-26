2c3e04414f31b132f0512b15b406837ab6df605d
BoykoNeov <boikoneov@gmail.com>
2017-05-27 00:39:27 +0300

    :seedling: Finished some additional tasks from "loops and cond statements"

---
+++ CondStatementsAndLoops/FiveDifferentNumbers/Program.cs
@@ -0,0 +1,45 @@
+﻿/// <summary>
+/// Problem 11.	5 Different Numbers
+/// You will be given two numbers – a and b.Generate five numbers - n1, n2, n3, n4, n5, for which the following conditions 
+/// are true: a ≤ n1&lt;n2&lt;n3&lt;n4 &lt; = n5 &lt; = b.If there is no number in the given interval, which satisfies the conditions – print “No!”.
+/// Input
+/// The input contains two integers, each on a new line.
+/// Output
+/// Print all numbers in increasing order and on a new line.
+/// Constraints
+/// a and b will be integers in the interval[-100…100]
+/// </summary>
+using System;
+
+class FiveDifferentNumbers
+{
+    public static void Main()
+    {
+        int firstNumber = int.Parse(Console.ReadLine());
+        int secondNumber = int.Parse(Console.ReadLine());
+        bool resultExists = false;
+
+        for (int i1 = firstNumber; i1 < secondNumber - 3; i1++)
+        {
+            for (int i2 = i1 + 1; i2 < secondNumber - 2; i2++)
+            {
+                for (int i3 = i2 + 1; i3 < secondNumber - 1; i3++)
+                {
+                    for (int i4 = i3 + 1; i4  < secondNumber; i4 ++)
+                    {
+                        for (int i5 = i4 + 1; i5 <= secondNumber; i5++)
+                        {
+                            Console.WriteLine($"{i1} {i2} {i3} {i4} {i5}");
+                            resultExists = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (!resultExists)
+        {
+            Console.WriteLine("No");
+        }
+    }
+}
---
+++ CondStatementsAndLoops/TestNumbers/TestNumbers.cs
@@ -0,0 +1,47 @@
+﻿using System;
+public class TestNumbers
+{
+    static void Main()
+    {
+        byte firstNumber = byte.Parse(Console.ReadLine());
+        byte secondNumber = byte.Parse(Console.ReadLine());
+        int sumBoundary = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+        int totalCombinations = 0;
+        bool isBoundaryPassed = false;
+
+        for (int i = firstNumber; i >= 1; i--)
+        {
+            for (int j = 1; j <= secondNumber; j++)
+            {
+                if (sum >= sumBoundary)
+                {
+                    isBoundaryPassed = true;
+                    break;
+                }
+                else
+                {
+                    totalCombinations++;
+                    sum += 3 * (i * j);
+                }
+            }
+            if (sum >= sumBoundary)
+            {
+                isBoundaryPassed = true;
+                break;
+            }
+        }
+
+        Console.WriteLine("{0} combinations", totalCombinations);
+
+        if (isBoundaryPassed)
+        {
+            Console.WriteLine("Sum: {0} >= {1}", sum, sumBoundary);
+        }
+        else
+        {
+            Console.WriteLine("Sum: {0}", sum);
+        }
+    }
+}
