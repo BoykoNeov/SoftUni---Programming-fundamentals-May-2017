bdb12229dffd389a72f7f43edd9ffc06db5071d3
BoykoNeov <boikoneov@gmail.com>
2017-06-10 23:57:15 +0300

    :hourglass: Started work on tasks from "Arrays and Methods - More Excercises"

---
+++ Arrays/ArrayStatistics/ArrayStatistics.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+    public class ArrayStatistics
+    {
+        public static void Main()
+        {
+        int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Console.WriteLine(input.Min());
+        }
+    }
